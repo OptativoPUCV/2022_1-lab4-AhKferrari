615d9dd31370a0d74a59c1497a1e533e5e67bd14
ahkferrari <>
2022-04-08 16:18:16 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -61,7 +61,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
-    map->buckets = malloc(sizeof(map->capacity)*2);
+    
 
 
 }
