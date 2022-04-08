a4d36b1be2d25bce9bc2d0c0e3d64ea13dbafd34
ahkferrari <>
2022-04-08 16:26:05 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -43,7 +43,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 
   int pos;
 
-  pos = hash(key);
+  pos = hash(key,map->capacity);
   while(map->buckets[pos] != NULL && map->buckets[pos]->key != NULL){
     pos = (pos+1)% map->capacity;
   }  
