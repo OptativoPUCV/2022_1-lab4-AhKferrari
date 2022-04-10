b471af13d17e7051c54c857e6db283079fee4ee9
ahkferrari <>
2022-04-10 17:53:50 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -94,7 +94,8 @@ Pair * searchMap(HashMap * map,  char * key) {
     return map->buckets[pos];
     }
   else {
-    while (map->buckets[pos] != NULL && is_equal(map->buckets[pos]->key,             key) == 0){
+    while (map->buckets[pos] != NULL && 
+          is_equal(map->buckets[pos]->key,key) == 0){
       pos = (pos+1)% map->capacity;
     }
     map->current = pos;  
