a378761dc62e60c57911a80522a51441e2c3b2fd
ahkferrari <>
2022-04-11 03:04:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -57,7 +57,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 
 void enlarge(HashMap * map) {
   enlarge_called = 1; //no borrar (testing purposes)
-  //////////////////////////////////////////// 
+
   int i;
   
   int cap2 = map->capacity;
