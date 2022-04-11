a430ab78554c3c28cb79027d9db59578c455ec15
ahkferrari <>
2022-04-11 00:12:42 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -120,7 +120,7 @@ Pair * firstMap(HashMap * map) {
 
   int pos;
   
-  
+  if (map->buckets[0] != NULL) return map->buckets[0];
   while (map->buckets[pos] == NULL){
     pos = (pos+1)% map->capacity;  
   }
