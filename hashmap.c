6fc28796519e12309dc9ff3d13dab7b2cc4ca259
ahkferrari <>
2022-04-11 00:22:46 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -119,8 +119,6 @@ Pair * searchMap(HashMap * map,  char * key) {
 Pair * firstMap(HashMap * map) {
 
   int pos = 0;
-  
-  if (map->buckets[0] != NULL) return map->buckets[0];
   while (map->buckets[pos] == NULL || map->buckets[pos]->key == NULL){
     pos = (pos+1)% map->capacity;  
   }
